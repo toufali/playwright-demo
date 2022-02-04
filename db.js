111532b7222933b5a03501b29a15824df7bf82d4
Amri Toufali <amri@handsomemedia.com>
2022-02-04 14:43:22 -0800

    connect via database url for heroku compatibility

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ import 'dotenv/config.js'
 import pg from 'pg'
 
 const { Pool } = pg
-const pool = new Pool()
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
 
 export default {
   query: (text, params) => pool.query(text, params)
